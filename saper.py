58d8c3f62e50bca4ed80f1986d8396a035bdabba
KacperPBT <148689431+KacperPBT@users.noreply.github.com>
2024-03-05 10:49:37 +0100

    Update saper.py

--- saper.py
+++ saper.py
@@ -1,68 +1,52 @@
-"""
-pole 9x9
-bomby 5
-"""
-from datetime import datetime
 from random import randrange
-NOW = datetime.now()
-NOW = NOW.microsecond
+
 def array_printer(array:list):
     cols = len(array)
     for i in range(cols):
         print(array[i])
+# draft = [[0, 0, 0, 0, 1, 9, 1, 0, 0],[1, 1, 1, 0, 1, 1, 1, 0, 0],[1, 9, 1, 0, 1, 1, 1, 0, 0],[1, 1, 1, 0, 1, 9, 1, 0, 0],[0, 0, 0, 0, 1, 2, 2, 1, 0],[0, 1, 1, 2, 1, 2, 9, 1, 0],[1, 2, 9, 4, 9, 4, 2, 2, 0],[1, 9, 3, 9, 9, 3, 9, 1, 0],[1, 1, 2, 2, 2, 2, 1, 1, 0]]
 
-draft = [[0, 0, 0, 0, 1, 9, 1, 0, 0],[1, 1, 1, 0, 1, 1, 1, 0, 0],[1, 9, 1, 0, 1, 1, 1, 0, 0],[1, 1, 1, 0, 1, 9, 1, 0, 0],[0, 0, 0, 0, 1, 2, 2, 1, 0],[0, 1, 1, 2, 1, 2, 9, 1, 0],[1, 2, 9, 4, 9, 4, 2, 2, 0],[1, 9, 3, 9, 9, 3, 9, 1, 0],[1, 1, 2, 2, 2, 2, 1, 1, 0]]
+class Saper_draft:
+    def __init__(self, field_size : int, bombs_number : int) -> None:
+        if field_size * field_size - bombs_number < 1:
+            raise ValueError("Too many bombs, they won't fit in field of this size")
+        self.field_size = field_size
+        self.bombs_number = bombs_number
 
+    def array_filler(self, filer) -> list:
+        array = []
+        for i in range(self.field_size):
+            sub_array = []
+            for j in range(self.field_size):
+                sub_array.append(filer)
+            array.append(sub_array)
+        return array
 
+    def rand_bomb_locat(self, field : list) -> list:
+        for i in range(self.bombs_number):
+            while True:
+                x = randrange(self.field_size)
+                y = randrange(self.field_size)
+                if field[x][y] == 0:
+                    break
+            field[x][y] = 9
+        return field
 
-def array_filler(cols : int, rows : int, filer) -> list:
-    array = []
-    for i in range(cols):
-        sub_array = []
-        for j in range(rows):
-            sub_array.append(filer)
-        array.append(sub_array)
-    return array
+    def bomb_suraunding(self, field : list) -> list:
+        for i in range(self.field_size):
+            for j in range(self.field_size):
+                if field[i][j] == 9:
+                    for k in range(8):
+                        h = [[i-1,j+1],[i,j+1],[i+1,j+1],[i-1,j],[i+1,j],[i-1,j-1],[i,j-1],[i+1,j-1]]
+                        x = h[k][0]
+                        y = h[k][1]
+                        if x >=0 and x < 9 and y>=0 and y < 9:
+                            if field[x][y] != 9:
+                                field[x][y] = field[x][y] + 1
+        return field
 
-def rand_bomb_locat(field : list, field_size : int) -> list:
-    for i in range(10):
-        while True:
-            x = randrange(field_size)
-            y = randrange(field_size)
-            if field[x][y] == 0:
-                break
-        field[x][y] = 9
-    return field
-
-def bomb_suraunding(field : list, field_size : int) -> list:
-    for i in range(field_size):
-        for j in range(field_size):
-            if field[i][j] == 9:
-                for k in range(8):
-                    h = [[i-1,j+1],[i,j+1],[i+1,j+1],[i-1,j],[i+1,j],[i-1,j-1],[i,j-1],[i+1,j-1]]
-                    x = h[k][0]
-                    y = h[k][1]
-                    if x >=0 and x < 9 and y>=0 and y < 9:
-                        if field[x][y] != 9:
-                            field[x][y] = field[x][y] + 1
-    return field
-
-
-
-
-
-
-
-
-
-
-
-pole = array_filler(9,9,0)
-pole = rand_bomb_locat(pole,9)
-pole = bomb_suraunding(pole,9)
-array_printer(pole)
-hpw = datetime.now()
-print(hpw.microsecond-NOW)
-
-print("==============================")
-array_printer(draft)
+    def make_draft(self):
+        field = self.array_filler(0)
+        field = self.rand_bomb_locat(field)
+        self.field = self.bomb_suraunding(field)
+        return self.field
